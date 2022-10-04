0b82f7fe515194ee2ce38c28e5133463e6d3732a
Yash Bhansali <bhansaliyashg@gmail.com>
2022-10-04 02:08:11 -0400

    added home view

--- apps/registration/views.py
+++ apps/registration/views.py
@@ -6,6 +6,13 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 
+class HomeView(View):
+    template_name = "registration/home.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template_name, context)
+
 class UserRegistrationView(FormView):
     template_name = "registration/user_registration.html"
     form_class = RegistrationForm
