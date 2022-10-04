047b7ebe86031413f6bcb1a4bab45290e886c8eb
Priyam Garg <gargpriyam21@gmail.com>
2022-10-04 02:00:15 -0400

    added user registration view

--- apps/registration/views.py
+++ apps/registration/views.py
@@ -1,2 +1,16 @@
+from django.shortcuts import render
+from .forms import RegistrationForm
+from django.views import View
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class UserRegistrationView(FormView):
+    template_name = "registration/user_registration.html"
+    form_class = RegistrationForm
+    success_url = reverse_lazy(viewname="registration:login")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
