bade0f8d1b92ded81fc8fdabf6b7e36bd73ee3ee
Priyam Garg <gargpriyam21@gmail.com>
2022-10-05 01:32:22 -0400

    Added URLpatterns

--- apps/registration/urls.py
+++ apps/registration/urls.py
@@ -1,4 +1,15 @@
 from django.urls import path
 
+from .views import (
+    UserRegistrationView,
+    HomeView,
+)
+from django.contrib.auth import views as auth_views
+
 app_name = "registration"
-urlpatterns = []
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+    path("register/", UserRegistrationView.as_view(), name="user-registration"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+]
