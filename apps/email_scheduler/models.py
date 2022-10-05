738ed1ad91eca2d1417d605d3de5703071361c8c
Divyang Doshi <divyangdoshi02@gmail.com>
2022-10-05 00:36:37 -0400

    adding helper function for email scheduler model

--- apps/email_scheduler/models.py
+++ apps/email_scheduler/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from .tasks import send_email
 from .constants import *
 from django.utils import timezone
 from django.db.models import F, Q
@@ -57,6 +58,67 @@ class EmailScheduler(models.Model):
         blank=True,
         help_text="If task failed, information regarding why it failed",
     )
+
+    @classmethod
+    def pending_periodic_email_finder(cls):
+        """This method finds all the EmailScheduler objects which need to be sent in case of periodic task"""
+
+        pending_emails = (
+            cls.objects.select_for_update()
+            .exclude(email_repeat_after__exact=None, email_last_sent_at__exact=None)
+            .filter(
+                task_status__exact=TASK_STATUS_PENDING,
+                email_last_sent_at__lte=timezone.now() - F("email_repeat_after"),
+            )
+        )
+        return pending_emails
+
+    def pre_update_processing(self):
+        """
+        The purpose of this method is, to do preprocessing before updating an object.
+        Currently, when we are updating an EmailScheduler object, and we are changing
+        the email_repeat_after to None then we should automatically mark the task complete
+        if it's not already in Failed state. Here we first fetch the object from db which contains the old state
+        before updating and self contains the state after updating, hence db call is necessary.
+        """
+        old_obj = EmailScheduler.objects.get(pk=self.pk)
+        if (
+            old_obj.email_repeat_after is not None
+            and self.email_repeat_after is None
+            and old_obj.task_status != TASK_STATUS_FAILED
+        ):
+            self.task_status = TASK_STATUS_COMPLETE
+
+    def save(self, *args, **kwargs):
+        """
+        We have overrided the save method to call respective send email method i.e send immidiately or
+        after specified duration. We also call pre_update_processing to update the obejct properly in case of
+        update.
+        """
+        is_adding_new = self._state.adding
+        if not is_adding_new:
+            self.pre_update_processing()
+        super(EmailScheduler, self).save(*args, **kwargs)
+        if is_adding_new:
+            if self.email_schedule is None:
+                send_email.delay(self.pk)
+            elif self.email_schedule is not None:
+                td = self.email_schedule
+                send_email.apply_async(
+                    kwargs={"email_scheduler_obj_id": self.pk}, eta=td
+                )
+
+    def update_fields(self, updated_fields: dict):
+        """
+        This method takes the dict of the updated keys in the model. It updates the
+        corresponding fields in the model instance and saves it in db.
+        """
+        update_fields = []
+        for key, value in updated_fields.items():
+            setattr(self, key, value)
+            update_fields.append(key)
+        self.save(update_fields=update_fields)
+
 
 
 class EmailSchedulerLogs(models.Model):
