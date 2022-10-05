bdf738404878a4eb0c6b3def6b6949b4dd26b964
Divyang Doshi <divyangdoshi02@gmail.com>
2022-10-05 00:56:29 -0400

    add email scheduler service

--- apps/email_scheduler/services.py
+++ apps/email_scheduler/services.py
@@ -17,7 +17,49 @@ class EmailService:
             if sender_class.email_service_used() == sender_name:
                 return sender_class
 
-    
+    @classmethod
+    def _update_email_scheduler(
+        cls,
+        email_scheduler_obj,
+        parsed_response: list,
+        max_retries: int,
+        retry_count: int,
+        success: bool,
+    ):
+        """This method is used to update email scheduler object after we send email"""
+        if success:
+            updated_fields = {
+                "email_last_sent_at": timezone.now(),
+                "email_send_count": email_scheduler_obj.email_send_count + 1,
+            }
+            if email_scheduler_obj.email_repeat_after is None:
+                updated_fields["task_status"] = TASK_STATUS_COMPLETE
+            email_scheduler_obj.update_fields(updated_fields)
+        else:
+            updated_fields = {
+                "task_failed_count": email_scheduler_obj.task_failed_count + 1
+            }
+
+            errors = parsed_response
+
+            error_429_or_500 = False
+            for err in errors:
+                if (
+                    err["StatusCode"] == HTTPStatus.TOO_MANY_REQUESTS
+                    or err["StatusCode"] == HTTPStatus.INTERNAL_SERVER_ERROR
+                ):
+                    error_429_or_500 = True
+                    break
+            updated_fields["task_failure_info"] = errors
+
+            if retry_count < max_retries and error_429_or_500:
+                email_scheduler_obj.update_fields(updated_fields)
+                raise EmailSendingFailedWith429or500
+            else:
+                updated_fields["task_status"] = TASK_STATUS_FAILED
+                email_scheduler_obj.update_fields(updated_fields)
+
+
     @classmethod
     def send_email(
         cls, email_scheduler_obj_id: int, max_retries: int, retry_count: int
