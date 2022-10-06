a93e658d45eef31c9a6d51c9b6641def28500d5b
Manogna Choudary Potluri <manognacpotluri@gmail.com>
2022-10-06 01:43:14 -0400

    add teststo check update logs functionality

--- apps/email_scheduler/tests/test_models.py
+++ apps/email_scheduler/tests/test_models.py
@@ -51,3 +51,36 @@ class TestEmailSchedulerLogsModel(TestCase):
         EmailSchedulerLogs.create_logs(log_list)
         self.assertEqual(len(EmailSchedulerLogs.objects.all()), 1)
         self.assertEqual(EmailSchedulerLogs.objects.all()[0].email_message_id, "1")
+
+    def test_get_logs_to_be_updated(self):
+        email_scheduler = EmailScheduler.objects.create(
+            email_to="abc@gmail.com",
+            email_subject="Test subject 2 ",
+            email_body="Test email body 2",
+        )
+        email_scheduler_log = EmailSchedulerLogs.objects.create(
+            email_scheduler=email_scheduler,
+            email_recipient_id="abc@gmail.com",
+            email_message_id="1",
+            email_recipient_type=EMAIL_RECIPIENT_TYPE_TO,
+        )
+
+        queryset = EmailSchedulerLogs.get_logs_to_be_updated()
+        self.assertEqual(len(queryset), 1)
+        self.assertEqual(queryset[0].email_message_id, "1")
+
+    def test_email_scheduler_logs_updater(self):
+        email_scheduler = EmailScheduler.objects.create(
+            email_to="abc@gmail.com",
+            email_subject="Test subject 2 ",
+            email_body="test",
+        )
+        email_scheduler_log_to_be_updated = EmailSchedulerLogs.objects.create(
+            email_scheduler=email_scheduler,
+            email_recipient_id="abc@gmail.com",
+            email_message_id="1",
+            email_recipient_type=EMAIL_RECIPIENT_TYPE_TO,
+        )
+        email_scheduler_log_to_be_updated.update_fields({"email_send_status": "sent"})
+        email_scheduler_log_to_be_updated.refresh_from_db()
+        self.assertEqual(email_scheduler_log_to_be_updated.email_send_status, "sent")
