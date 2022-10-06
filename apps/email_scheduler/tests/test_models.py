740d0c5dc17f2624100bcc7efe803c5145c2b96f
Manogna Choudary Potluri <manognacpotluri@gmail.com>
2022-10-06 01:45:17 -0400

    add additional tests for email schedular model helpers

--- apps/email_scheduler/tests/test_models.py
+++ apps/email_scheduler/tests/test_models.py
@@ -33,6 +33,60 @@ class TestEmailSchedulerModel(TestCase):
                 eta=email_scheduler_new.email_schedule,
             )
 
+    def test_update_repeat_to_none_marks_task_complete(self):
+        with patch("apps.email_scheduler.models.send_email.delay") as mock_task:
+            email_scheduler_new = EmailScheduler.objects.create(
+                email_to="abc@gmail.com",
+                email_subject="Test subject 2 ",
+                email_body="Test email body 2",
+                email_repeat_after=timezone.timedelta(minutes=1),
+            )
+            email_scheduler_new.email_repeat_after = None
+            email_scheduler_new.save()
+            email_scheduler_new.refresh_from_db()
+            self.assertEqual(email_scheduler_new.email_repeat_after, None)
+            self.assertEqual(email_scheduler_new.task_status, TASK_STATUS_COMPLETE)
+
+    def test_updating_failed_task_does_not_complete(self):
+        with patch("apps.email_scheduler.models.send_email.delay") as mock_task:
+            email_scheduler_new = EmailScheduler.objects.create(
+                email_to="abc@gmail.com",
+                email_subject="Test subject 2 ",
+                email_body="Test email body 2",
+                email_repeat_after=timezone.timedelta(minutes=1),
+                task_status=TASK_STATUS_FAILED,
+            )
+            email_scheduler_new.email_repeat_after = None
+            email_scheduler_new.save()
+            email_scheduler_new.refresh_from_db()
+            self.assertEqual(email_scheduler_new.email_repeat_after, None)
+            self.assertNotEqual(email_scheduler_new.task_status, TASK_STATUS_COMPLETE)
+
+    def test_pending_email_finder(self):
+        periodic_email_scheduler = EmailScheduler.objects.create(
+            email_to="abc@gmail.com",
+            email_subject="Test subject 2 ",
+            email_body="test",
+            email_repeat_after=timezone.timedelta(seconds=1),
+            email_last_sent_at=timezone.now(),
+        )
+        sleep(1)
+        queryset = EmailScheduler.pending_periodic_email_finder()
+        self.assertEqual(len(queryset), 1)
+        self.assertEqual(queryset[0].pk, periodic_email_scheduler.pk)
+
+    def test_email_scheduler_updater(self):
+        email_scheduler_to_be_updated = EmailScheduler.objects.create(
+            email_to="abc@gmail.com",
+            email_subject="Test subject 2 ",
+            email_body="test",
+            email_repeat_after=timezone.timedelta(seconds=1),
+            email_last_sent_at=timezone.now(),
+        )
+        email_scheduler_to_be_updated.update_fields({"email_send_count": 1})
+        email_scheduler_to_be_updated.refresh_from_db()
+        self.assertEqual(email_scheduler_to_be_updated.email_send_count, 1)
+
 class TestEmailSchedulerLogsModel(TestCase):
     def test_create_logs(self):
         email_scheduler = EmailScheduler.objects.create(
