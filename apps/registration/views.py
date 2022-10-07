9cb09cc8c03b1c4912859a2b5eb9e2d2d6c8bad3
Priyam Garg <gargpriyam21@gmail.com>
2022-10-07 18:59:26 -0400

    Adding documentation for views.py

--- apps/registration/views.py
+++ apps/registration/views.py
@@ -6,18 +6,42 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 
+"""
+This module will define views for Registration app
+"""
+
 class HomeView(View):
+    """
+    This class will define the home view
+    """
     template_name = "registration/home.html"
 
     def get(self, request, *args, **kwargs):
+        """
+        This method will render the home page
+        Arguments:
+            request {object} -- Request object
+        Returns:
+            object -- HttpResponse object
+        """
         context = {}
         return render(request, self.template_name, context)
 
 class UserRegistrationView(FormView):
+    """
+    This class will define the user registration view
+    """
     template_name = "registration/user_registration.html"
     form_class = RegistrationForm
     success_url = reverse_lazy(viewname="registration:login")
 
     def form_valid(self, form):
+        """
+        This method will save the form and redirect to login page
+        Arguments:
+            form {object} -- Form object
+        Returns:
+            object -- HttpResponse object
+        """
         form.save()
         return super().form_valid(form)
