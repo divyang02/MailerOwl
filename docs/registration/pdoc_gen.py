0c53d0f557bf57df55f733f3bce47ea6a88d727d
divyang02 <divyangdoshi02@gmail.com>
2022-10-08 08:45:11 +0000

    Automated autoyapf fixes

--- docs/registration/pdoc_gen.py
+++ docs/registration/pdoc_gen.py
@@ -8,25 +8,26 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mail_sender.local_settings')
 
 # Setup Django
 import django
-django.setup()
 
+django.setup()
 
 import pdoc
 
 modules = ['apps.registration']
 context = pdoc.Context()
 
-modules = [pdoc.Module(mod, context=context)
-           for mod in modules]
+modules = [pdoc.Module(mod, context=context) for mod in modules]
 pdoc.link_inheritance(context)
+
 
 def recursive_htmls(mod):
     yield mod.name, mod.html()
     for submod in mod.submodules():
         yield from recursive_htmls(submod)
 
+
 for mod in modules:
     for module_name, html in recursive_htmls(mod):
-        module_name  = module_name.split(".")[-1]
+        module_name = module_name.split(".")[-1]
         with open(f"{module_name}.html", "w", encoding="utf8") as f:
             f.write(html)
